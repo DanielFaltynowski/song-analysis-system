b9e20d37913b2e4def7aafae8d6bc13257820726
Daniel <faltynowskidaniel6@gmail.com>
2023-12-17 16:13:55 +0100

    fix

--- songs_recommendation/server/app.py
+++ songs_recommendation/server/app.py
@@ -49,7 +49,10 @@ def get_access():
 
 
 # PUNKT 4
-@app.route("/song/like/<songid>", methods=['GET'])
+
+
+
+@app.route("/songs/song/like/<songid>", methods=['GET'])
 def like_song(songid):
     with driver.session() as session:
         def session_like_song(tx, songid):
@@ -69,7 +72,7 @@ def like_song(songid):
     return {"message": "success"}
 
 
-@app.route("/song/unlike/<songid>", methods=['POST'])
+@app.route("/songs/song/unlike/<songid>", methods=['POST'])
 def unlike_song(songid):
     with driver.session() as session:
         def session_unlike_song(tx, songid):
@@ -88,7 +91,7 @@ def unlike_song(songid):
     return {"message": "success"}
 
 
-@app.route("/song/love/<songid>", methods=['GET'])
+@app.route("/songs/song/love/<songid>", methods=['GET'])
 def love_song(songid):
     with driver.session() as session:
         def session_love_song(tx, songid):
@@ -107,7 +110,7 @@ def love_song(songid):
     return {"message": "success"}
 
 
-@app.route("/song/unlove/<songid>", methods=['GET'])
+@app.route("/songs/song/unlove/<songid>", methods=['GET'])
 def unlove_song(songid):
     with driver.session() as session:
         def session_unlove_song(tx, songid):
@@ -126,7 +129,7 @@ def unlove_song(songid):
     return {"message": "success"}
 
 
-@app.route("/song/hate/<songid>", methods=['GET'])
+@app.route("/songs/song/hate/<songid>", methods=['GET'])
 def hate_song(songid):
     with driver.session() as session:
         def session_hate_song(tx, songid):
@@ -145,7 +148,7 @@ def hate_song(songid):
     return {"message": "success"}
 
 
-@app.route("/song/unhate/<songid>", methods=['GET'])
+@app.route("/songs/song/unhate/<songid>", methods=['GET'])
 def unhate_song(songid):
     with driver.session() as session:
         def session_unhate_song(tx, songid):
@@ -164,7 +167,7 @@ def unhate_song(songid):
     return {"message": "success"}
 
 
-@app.route('/song/<songid>', methods=['GET'])
+@app.route('/songs/song/<songid>', methods=['GET'])
 def get_song_by_id(songid):
     with driver.session() as session:
         def session_get_song_by_id(tx, songid):
@@ -172,6 +175,47 @@ def get_song_by_id(songid):
                 """
                     MATCH (s:Song {id: toInteger($songid)})
                     RETURN s;
+                """,
+                songid=songid
+            )
+
+            return [ record["s"] for record in result ]
+        
+        songs = session.execute_read(session_get_song_by_id, songid=songid)
+
+        session.close()
+
+    response_body = {
+        "songs": [
+            {
+                "id": song["id"],
+                "title": song["title"],
+                "likes": song["likes"],
+                "loves": song["loves"],
+                "hates": song["hates"],
+                "compound": song["sentiment_compound"],
+                "angry": song["emotion_Angry"],
+                "happy": song["emotion_Happy"],
+                "surprise": song["emotion_Surprise"],
+                "sad": song["emotion_Sad"],
+                "sentiment": song["sentiment_category"],
+                "views": song["views"]
+            } for song in songs
+        ]
+    }
+
+    response = jsonify(response_body)
+    return response
+
+
+@app.route('/songs', methods=['GET'])
+def get_song_by_id(songid):
+    with driver.session() as session:
+        def session_get_song_by_id(tx, songid):
+            result = tx.run(
+                """
+                    MATCH (s:Song)
+                    RETURN s LIMIT 25;
                 """,
                 songid=songid
             )
@@ -236,7 +280,7 @@ def get_favourite_songs_by_user(userid):
 
 
 # PUNKT 6
-@app.route("/artist/<artist>", methods=['GET'])
+@app.route("/songs/artist/<artist>", methods=['GET'])
 def get_songs_by_artist(artist):
     with driver.session() as session:
         def session_get_songs_by_artist(tx, artist):
@@ -264,7 +308,7 @@ def get_songs_by_artist(artist):
 
 
 # PUNKT 7
-@app.route("/tag/<tag>", methods=['GET'])
+@app.route("/songs/tag/<tag>", methods=['GET'])
 def get_songs_by_tag(tag):
     with driver.session() as session:
         def session_get_songs_by_tag(tx, tag):
