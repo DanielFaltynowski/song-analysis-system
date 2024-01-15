c6c1953ec610078360a1c3bfcef1a384e4c62f0d
Honorata7 <h.kucharska.690@studms.ug.edu.pl>
2024-01-15 09:30:47 +0100

    fix

--- songs_recommendation/client/src/pages/Song.js
+++ songs_recommendation/client/src/pages/Song.js
@@ -5,37 +5,9 @@ import unlikeImage from './unlike.png';
 
 const Song = ({ id, title, artist }) => {
 
-  const likeSong = async (e) => {
-    e.stopPropagation();  // Stop the click event propagation
-    try {
-      const response = await axios.post(`http://127.0.0.1:5000/songs/song/like/${id}`, { songId: id });
-      
-      console.log('Response from the server:', response.data);
-      
-      if (response.status === 200) {
-        alert('Liked!');
-      } else {
-        console.error('Error adding to favorites. Unexpected status code:', response.status);
-        alert('Failed to add to favorites. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error adding to favorites:', error);
-  
-      if (error.response) {
-        console.error('Server responded with:', error.response.data);
-        console.error('Status code:', error.response.status);
-        console.error('Headers:', error.response.headers);
-      } else if (error.request) {
-        console.error('No response received from the server');
-      } else {
-        console.error('Error setting up the request:', error.message);
-      }
-  
-      alert('Failed to add to favorites. Please try again.');
-    }
-  };
-  
-  
+  const likeSong = async () => {
+    console.log('like song');
+  }
 
   const unlikeSong = async () => {
     console.log('unlike song');
